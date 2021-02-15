63d5b2b11c68bfcca7393e9710845983e090f972
Hank <horicky7@gmail.com>
2021-02-14 17:43:35 -0800

    radio-buttonoption

---
+++ src/radio/buttonoption.js
@@ -0,0 +1,50 @@
+import { RadioContext } from "./context";
+import PropTypes from "prop-types";
+import { SeaUIBase, SeaUIType } from "../_util/SeaUIBase";
+import "./buttonoption.scss";
+export class ButtonOption extends SeaUIBase {
+  static contextType = RadioContext;
+  constructor(props) {
+    super(props, SeaUIType.RADIO_BUTTONOPTION);
+    this.state = { value: props.value };
+  }
+  classNames() {
+    let { color, size, value, disable } = this.context;
+    return this.getClassNames(
+      ["seaui-radio-buttonoption", color, size],
+      {
+        "seaui-radio-buttonoption-selected": value == this.state.value,
+        "seaui-disable": disable || this.props.disable,
+      },
+      this.props.customClass
+    );
+  }
+
+  onClick = () => {
+    let { onchange, disable } = this.context;
+    if (!(disable || this.props.disable)) {
+      onchange(this.state.value);
+    }
+  };
+
+  render() {
+    return (
+      <label className={this.classNames()} onClick={this.onClick}>
+        {this.props.text}
+      </label>
+    );
+  }
+}
+
+ButtonOption.propTypes = {
+  text: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  customClass: PropTypes.string,
+  disable: PropTypes.bool,
+};
+
+ButtonOption.defaultProps = {
+  value: "",
+  customClass: "",
+  disable: false,
+};
